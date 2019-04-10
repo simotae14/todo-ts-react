df11988569456ee959e101993bc6f40fc01f8c67
Simone Taeggi <startae14@gmail.com>
2019-04-10 15:08:46 +0200

    Add the useState hook for the todo value input

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,28 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import ReactDOM from 'react-dom'
 
+// create an alias for the event type
+type FormElem = React.FormEvent<HTMLFormElement>
+
 export default function App(): JSX.Element {
-    const sum = (a:number, b:number) => a + b
+    // set the useState hook for the todo input content
+    const [todo, setTodoValue] = useState<string>('')
+
+    const handleSubmit = (e: FormElem):void => {
+        e.preventDefault();
+        setTodoValue('');
+    }
+
     return (
-        <h1>
-            Hello !! { sum(15, 15) }
-        </h1>
+        <Fragment>
+            <h1>
+                Todo List
+            </h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={todo} onChange={ e => setTodoValue(e.target.value) } required />
+                <button type="submit">Add Todo</button>
+            </form>
+        </Fragment>
     )
 }
 
